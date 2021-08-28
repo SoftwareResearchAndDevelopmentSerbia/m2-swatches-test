afa63485bb5a950e43681e00ae3975b15c9cd03f
Milan Cvetkovic <info@software-research-and-development.com>
2021-08-29 00:29:37 +0200

    feat: Sample Data installer module added for M2.4.2

---
+++ app/code/SoftwareResearchAndDevelopment/DataInstaller/registration.php
@@ -0,0 +1,8 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'SoftwareResearchAndDevelopment_DataInstaller',
+    __DIR__
+);
